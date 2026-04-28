1967476d40dc9c3e7e046cdd9a263ea4873d55f9
Spacetinker <davidrobertmorea@gmail.com>
2026-04-28 14:25:54 -0500

    added several upgrades to the skill tree and renamed a bunch of stuff

--- main.js
+++ main.js
@@ -7,13 +7,16 @@ let possibleEnemies = ["energy"];
 let treeId = 1;
 // temporary, make it settable later on
 let enemyTrees = [1, 2];
-let powertrees = [0, 0, 1, 1];
-let powerrequires = [0, 0, 0, 1];
-let powernames = ["scratch", "resistance", "spark", "fire"];
-let powerspower = [15, 10, 25, 15];
-let powereffect = ["1", "2", "1", "3"];
+let powerTrees = [0, 0, 1, 1, 1];
+let powerRequires = [0, 0, 0, 2, 2];
+let powerNames = ["scratch", "resistance", "spark", "fire", "lightning"];
+let powersPower = [15, 10, 25, 15, 50];
+let powerEffect = ["1", "2", "1", "3", "1"];
 let unlockedPowers = [0, 1];
-//3 is burning, meaning that without specific items, it will apply a burning effect and perform 1
+let exclusivePowers = [-1, -1, -1, 3, 2];
+let selectedPower = -1;
+let equippedPowers = [0, 1, -1, -1]
+//3 is burning, meaning that without specific items, it will apply a burning effect and perform 1 for the power
 //2 is defence in that the ability's power will increase block by that amount
 //1 is jsut damage in that it will damage the opponent by that amount
 
@@ -64,37 +67,93 @@ function openSkills(){
   document.getElementById("skillTree").hidden = false;
 }
 
+function closeSkill(){
+  document.getElementById("skillTree").hidden = true;
+}
+
 function loadSkills(){
   console.log("loaded skills")
-  console.log(powertrees.indexOf(treeId))
-  var i = powertrees.indexOf(treeId);
-  while(i != powertrees.lastIndexOf(treeId)+1){
+  console.log(powerTrees.indexOf(treeId))
+  var i = powerTrees.indexOf(treeId);
+  while(i != powerTrees.lastIndexOf(treeId)+1){
     console.log(i)
     if(unlockedPowers.includes(i)){
-      console.log(powernames[i] + " unlocked " + powerrequires[i])
-      document.getElementById(powernames[i]).style.backgroundColor = "white";
-    }else if (unlockedPowers.includes(powerrequires[i]) || powerrequires[i] == 0){
-      console.log(powernames[i] + " loaded " + powerrequires[i])
-      document.getElementById(powernames[i]).style.backgroundColor = "lightgray";
+      console.log(powerNames[i] + " unlocked " + powerRequires[i])
+      document.getElementById(powerNames[i]).style.backgroundColor = "white";
+      //83: Nothing here
+    }else if (unlockedPowers.includes(powerRequires[i]) || powerRequires[i] == 0){
+      console.log(powerNames[i] + " loaded " + powerRequires[i])
+      document.getElementById(powerNames[i]).style.backgroundColor = "lightgray";
     }
     i++;
   }
+  if (equippedPowers[0] == -1) {
+    document.getElementById("PowerTree1").innerHTML = " - - ";
+    document.getElementById("Power1").innerHTML = " - - ";
+  }else{
+    document.getElementById("PowerTree1").innerHTML = powerNames[equippedPowers[0]];
+    document.getElementById("Power1").innerHTML = powerNames[equippedPowers[0]];
+  }
+  if (equippedPowers[1] == -1) {
+    document.getElementById("PowerTree2").innerHTML = " - - ";
+    document.getElementById("Power2").innerHTML = " - - ";
+  }else{
+    document.getElementById("PowerTree2").innerHTML = powerNames[equippedPowers[1]];
+    document.getElementById("Power2").innerHTML = powerNames[equippedPowers[1]];
+  }
+  if (equippedPowers[2] == -1) {
+    document.getElementById("PowerTree3").innerHTML = " - - ";
+    document.getElementById("Power3").innerHTML = " - - ";
+  }else{
+    document.getElementById("PowerTree3").innerHTML = powerNames[equippedPowers[2]];
+    document.getElementById("Power3").innerHTML = powerNames[equippedPowers[2]];
+  }
+  if (equippedPowers[3] == -1) {
+    document.getElementById("PowerTree4").innerHTML = " - - ";
+    document.getElementById("Power4").innerHTML = " - - ";
+  }else{
+    document.getElementById("PowerTree4").innerHTML = powerNames[equippedPowers[3]];
+    document.getElementById("Power4").innerHTML = powerNames[equippedPowers[3]];
+  }
+  console.log(equippedPowers)
 }
 
 function learnSkill(skillId){
-  console.log(powerrequires[skillId])
+  console.log(powerRequires[skillId])
   if(unlockedPowers.includes(skillId) == false){
-  if(powerrequires[skillId] == 0){
+  if(powerRequires[skillId] == 0){
     unlockedPowers[unlockedPowers.length] = skillId;
     loadSkills();
-  }else if(unlockedPowers.includes(powerrequires[skillId])){
+  }else if(unlockedPowers.includes(powerRequires[skillId])){
     unlockedPowers[unlockedPowers.length] = skillId;
     loadSkills();
-  }}
+  }}else{
+    selectedPower = skillId;
+    document.getElementById("powerSelectInfo").innerHTML = powerNames[skillId] + " Selected";
+  }
   console.log(unlockedPowers);
 }
-//83 : nothing here
+
+function placePower(pos) {
+  if (selectedPower != -1){
+    if (equippedPowers.includes(selectedPower) == false){
+      equippedPowers[pos] = selectedPower;
+      console.log("succsessfully placed power" + selectedPower)
+    }
+    selectedPower = -1;
+    document.getElementById("powerSelectInfo").innerHTML = "None Selected";
+    loadSkills();
+  }
+}
+
 function startBattle(enemy) {
   document.getElementById("combatMenu").hidden = false;
   document.getElementById("nonCombatMenu").hidden = true;
 }
+
+//what are you looking for? there is nothing down here
+// - Spacetinker
+// imagine if this gets anywhere, that'd be cool
+// - Spacetinker again
+// i should stop doing these little messages at the bottom
+// - spacetinker again, i'm procrastinating and made 3 messages in a hour
